07d7ca26f1751859b4dadc6644fa07161269419a
rezahatampoor <rezahtms@gmail.com>
2024-01-28 23:51:24 +0330

    add style to travel list project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="app">
+      <Logo />
+      <Form />
+      <PackingList />
+      <Stats />
+    </div>
+  );
 }
+
+const Logo = () => {
+  return <h1>🌴 FAR AWAY 👜</h1>;
+};
+
+const Form = () => {
+  return (
+    <form className="add-form">
+      <h3>What Do You Need For Your 😍 Trip?</h3>
+    </form>
+  );
+};
+
+const PackingList = () => {
+  return <div className="list">List</div>;
+};
+
+const Stats = () => {
+  return (
+    <footer className="stats">
+      <em>You Have Got X Pack On YourList,And You Already Packed X (X%)</em>
+    </footer>
+  );
+};
 
 export default App;
